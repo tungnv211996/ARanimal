d91a67b949a8ee63fe79b62ac6ed012365ec5bc8
tungnv211996 <tungnv21199@gmail.com>
2017-12-21 01:17:42 +0700

    fix uri database

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@
 var configs = {
     // HOST: 'https://aranimal.herokuapp.com/',
     HOST: 'http://localhost:3000',
-    // mongoDb: 'mongodb://tungnv211996:NguyenVanTung123@ds135196.mlab.com:35196/aranimal',
-    MONGODB_URI: 'mongodb://localhost:27017/ARanimal'
+    mongoDb: 'mongodb://tungnv211996:NguyenVanTung123@ds135196.mlab.com:35196/aranimal',
+    // MONGODB_URI: 'mongodb://localhost:27017/ARanimal'
 }
 
 module.exports = configs
